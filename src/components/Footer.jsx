82a15c48f349093732c784a488808b1f9d4b4801
Daniel <danielamirshaev@gmail.com>
2025-04-04 20:38:24 +0300

    save

---
+++ src/components/Footer.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <article>
+        <p className="PTag">Contact Us</p>
+        <p className="PTag2">+778 456 893 0283</p>
+        <p className="PTag2">Sinseybeauty@gmail.com</p>
+        <p className="PTag2">
+          <div>Monday - Friday</div>
+          <div>10:00 am to 17:00 pm</div>
+        </p>
+        <span></span>
+        <p className="PTag5">© 2025 SINSEY Official. Powered by Shopify</p>
+      </article>
+      <article>
+        <p className="Ptag_1Art2">Shop</p>
+        <p className="Ptag_2Art2">About</p>
+        <p className="Ptag_2Art2">Product</p>
+        <p className="Ptag_2Art2">Reviews</p>
+        <p className="Ptag_2Art2">Contact</p>
+      </article>
+      <article>
+        <p className="PTag-1Art3">Newsletter</p>
+        <p className="ptag3">
+          Sign up for exclusive offers, original stories, events and more.
+        </p>
+      </article>
+    </footer>
+  );
+};
+
+export default Footer;
