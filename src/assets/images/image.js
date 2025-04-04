cecaad11f5b2be7ac2c091d3bd425930a74ea825
Aiganysh <dzhumagulova8@gmail.com>
2025-04-04 12:31:15 +0300

    create component media & adding images for banner, cardItem

---
+++ src/assets/images/image.js
@@ -0,0 +1,41 @@
+export { default as FirstBanner } from "./first image.jpg";
+export { default as SecondBanner } from "./second image.jpg";
+export { default as ThirdBanner } from "./third image.jpg";
+export { default as Sunscream } from "./крем от загара.webp";
+export { default as CreamAfterSun } from "./крем после загара.webp";
+export { default as SuntanOil } from "./Масло от загара.webp";
+export { default as SprayTanning } from "./Спрей для загара.webp";
+export { default as HydrophilicOil } from "./Гидроф масло зеленый чай.webp";
+export { default as MoisturizingMask } from "./Увлажняющая маска.webp";
+export { default as FacialTonic } from "./Тоник для лица.webp";
+export { default as MoisturizingCream } from "./увлажняющий крем.webp";
+export { default as MoisturizingSerum } from "./Сывортка увляжняющая.webp";
+export { default as AntiAgingSerum } from "./Антивозрастная сыворотка.webp";
+export { default as NightSerum } from "./Ночная сыворотка.webp";
+export { default as PerfecttSerum } from "./Сыворотка перфектор.webp";
+export { default as MaskAcids } from "./Маска с кислотами.webp";
+export { default as MattifyingTonic } from "./Матирующий тоник.webp";
+export { default as EnzymePowder } from "./Пудра энзимная.webp";
+export { default as LipScrub } from "./скраб-бальзам для губ.webp";
+
+export const Images = {
+  FirstBanner,
+  SecondBanner,
+  ThirdBanner,
+  Sunscream,
+  CreamAfterSun,
+  SuntanOil,
+  SprayTanning,
+  HydrophilicOil,
+  MoisturizingMask,
+  FacialTonic,
+  MoisturizingCream,
+  MoisturizingSerum,
+  AntiAgingSerum,
+  NightSerum,
+  PerfecttSerum,
+  MaskAcids,
+  MattifyingTonic,
+  EnzymePowder,
+  LipScrub,
+};
