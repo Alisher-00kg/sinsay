0ad56d6f99ff2c7ea69a5f2995b5411ee5517338
Aiganysh <dzhumagulova8@gmail.com>
2025-04-05 18:43:33 +0300

    create wish-list

---
+++ src/utils/constants/CardItem.js
@@ -0,0 +1,83 @@
+import {
+  FoundationBlazy1,
+  LipOil1,
+  LipOil4,
+  LipStick4,
+  Mascara1,
+  Powder2,
+} from "../../assets/images/images";
+
+export const wishList = [
+  {
+    id: 1,
+    image: LipOil1,
+    title: "MIXIT Make up Glazed oil gloss, shade pale pink, 5 мл",
+    price: 16.5,
+    isFavorite: false,
+    isBasket: false,
+  },
+  {
+    id: 2,
+    image: LipOil4,
+    title: "MIXIT Make up Lip Gloss Blossy Lips, 06, 5 ml",
+    price: 17.5,
+    isFavorite: false,
+    isBasket: false,
+  },
+  {
+    id: 3,
+    image: Mascara1,
+    title: "MIXIT Make Up Glam Doll Lash Mascara Make Up, 10 g",
+    price: 18.5,
+    isFavorite: false,
+    isBasket: false,
+  },
+  {
+    id: 4,
+    image: Powder2,
+    title: "MIXIT Make Up Compact Powder color 02, 10г",
+    price: 19.5,
+    isFavorite: false,
+    isBasket: false,
+  },
+  {
+    id: 5,
+    image: FoundationBlazy1,
+    title: "MIXIT Make Up Foundation Blazy Look, 01, 30 мл",
+    price: 19.5,
+    isFavorite: false,
+    isBasket: false,
+  },
+  {
+    id: 6,
+    image: LipStick4,
+    title: "MIXIT Make Up Lipstick Balm-Look, 07, 3 г",
+    price: 19.5,
+    isFavorite: false,
+    isBasket: false,
+  },
+];
+
+export const data = [
+  { id: "e1", subTitle: "Face care", faceCare: [] },
+  {
+    id: "e2",
+    subTitle: "Body care",
+    faceCare: [],
+  },
+  {
+    id: "e3",
+    subTitle: "Hair care",
+    faceCare: [],
+  },
+  {
+    id: "e4",
+    subTitle: "Hand care",
+    faceCare: [],
+  },
+  {
+    id: "e5",
+    subTitle: "Other",
+    faceCare: [],
+  },
+];
