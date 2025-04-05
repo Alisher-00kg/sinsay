177582e17d7cc7b2be74c5f77efbf40d3dacaf47
elmir <elmirbek2000kg>
2025-04-05 23:20:21 +0300

    created CartList

--- src/components/cartList/CartList.jsx
+++ src/components/cartList/CartList.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import styled from "styled-components";
-import { CartItem } from "../UI/CartItem";
-import { Button } from "../UI/Button";
-import Footer from "../Footer";
+import { CartItem } from "./UI/CartItem";
+import { Button } from "./UI/Button";
+import Footer from "./Footer";
 styled;
 
 const CartList = () => {
