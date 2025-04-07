7044e98e2f88cc1b6b2238963234787e55e3e541
Aiganysh <dzhumagulova8@gmail.com>
2025-04-07 15:12:01 +0300

    add a logo & remove comp at App

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 import React from "react";
-import { Header } from "./components/Header";
 const App = () => {
-  return <div>
-    <Header/>
-  </div>;
+  return <div>App</div>;
 };
 
 export default App;
