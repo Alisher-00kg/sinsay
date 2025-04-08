2a707609f6fb0a054f4770eaec5097b8f93e6366
Aiganysh <dzhumagulova8@gmail.com>
2025-04-08 21:52:20 +0300

    create the product context

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { AuthProvider } from './context/AuthContext.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { AuthProvider } from "./context/AuthContext.jsx";
+import { ProductProvider } from "./context/ProductContext.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthProvider>
-    <App />
+      <ProductProvider>
+        <App />
+      </ProductProvider>
     </AuthProvider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
