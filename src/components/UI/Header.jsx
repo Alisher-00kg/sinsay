86552a11ac86ebf0f364e3986b50b9ea188b4096
Aiganysh <dzhumagulova8@gmail.com>
2025-04-10 17:35:38 +0300

    adding hover

--- src/components/UI/Header.jsx
+++ src/components/UI/Header.jsx
@@ -79,9 +79,13 @@ const ModalContent = styled.div`
   text-align: center;
 `;
 const StyledP = styled.p`
-  font-size: 10px;
+  font-size: 11px;
   text-align: left;
   cursor: pointer;
+  &:hover {
+    font-size: 12px;
+    color: gray;
+  }
 `;
 const ContainerHeader = styled.div`
   width: 90%;
